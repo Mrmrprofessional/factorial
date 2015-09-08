23230d00bcb2e13321ea45d395f1b1931464b6b1
Mike Rapp & Kyle Pratuch <student@epicodus.com>
2015-09-08 14:37:23 -0700

    finish factorial exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,18 @@
-var triangleTracker = function(side1, side2, side3) {
-    var sides = [side1, side2, side3];
-    sides.sort(function(a, b) {return b-a});
-
-    if (sides[0] >= sides[1] + sides[2]) {
-        return "Not a triangle, jerk.";
-    } else if ((side1 === side2) && (side1 === side3)) {
-        return "Equilateral";
-    } else if ((side1 === side2) || (side1 === side3) || (side2 === side3)) {
-        return "Isosceles";
+var factorial = function(number) {
+    if (number <= 0) {
+        return 1;
     } else {
-        return "Scalene";
+        var outcome = number*factorial(number-1);
+        return outcome;
     }
 };
 
 $(document).ready(function() {
-    $("form#triangle").submit(function(event) {
-        
-        var side1 = parseInt($("input#side1").val());
-        var side2 = parseInt($("input#side2").val());
-        var side3 = parseInt($("input#side3").val());
+    $("form#factorial").submit(function(event) {
+        var number = parseInt($("input#number").val());
+        var answer = factorial(number);
 
-        var result = triangleTracker(side1, side2, side3);
-
-        $(".type").text(result);
+        $(".answer").text(answer);
 
         $("#result").show();
         event.preventDefault();
--- spec/specs.js
+++ spec/specs.js
@@ -1,18 +1,10 @@
-describe('triangleTracker', function() {
+describe('factorial', function() {
 
-    it("is not a triangle if one side is greater than the sum of the others", function() {
-        expect(triangleTracker(5,2,2)).to.equal('not a triangle');
+    it('is 1 if the number is 0', function() {
+        expect(factorial(0)).to.equal(1);
     });
 
-    it("is equilateral for three sides that are all equal", function() {
-        expect(triangleTracker(5,5,5)).to.equal('equilateral');
+    it('is 3 if number is 6', function() {
+        expect(factorial(3)).to.equal(6);
     });
-
-    it("is isosceles for two sides that are equal", function() {
-        expect(triangleTracker(5,5,3)).to.equal('isosceles');
-    });
-
-    it("is scalene if no sides are equal", function() {
-        expect(triangleTracker(5,4,3)).to.equal('scalene');
-    })
 });
